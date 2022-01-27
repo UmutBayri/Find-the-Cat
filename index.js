9b15754e13a2deda6caaa243ab90906f82734de8
Umut Bayri <bayriumut8016@gmail.com>
2022-01-27 14:00:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,86 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
+class FindtheCat extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          name : this.props.name,
+          boxes : [true, false, false],
+      };
+  }
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name : this.props.name,
-            boxes : [true, false, false],
-        };
-    }
-
-    render() {
-        return (
-        <div>
-        <h1 
-        style = {{
-            textAlign : "center",
-            border: "1px solid black",
-            marginLeft : "auto",
-            marginRight:"auto"
-        }}>
-        {this.state.name}
-        </h1>
-            <table style = {{
-                textAlign : "center",
-                border: "1px solid black",
-                marginLeft : "auto",
-                marginRight:"auto"
-            }}>
-                <tr>
-                <th><CreateButton name = "Box 1" truth = {this.state.boxes[0]} /></th>
-                <th><CreateButton name = "Box 2" truth = {this.state.boxes[1]} /></th>
-                <th><CreateButton name = "Box 3" truth = {this.state.boxes[2]} /></th>
-                </tr>
-            </table>
-        </div>
-        );
-    }
+  render() {
+      return (
+      <div>
+      <h1 className = "Title">
+      {this.state.name}
+      </h1>
+          <table className = "Table" >
+              <tr>
+              <th><CreateButton name = "Box 1" truth = {this.state.boxes[0]} /></th>
+              <th><CreateButton name = "Box 2" truth = {this.state.boxes[1]} /></th>
+              <th><CreateButton name = "Box 3" truth = {this.state.boxes[2]} /></th>
+              </tr>
+          </table>
+      </div>
+      );
+  }
 }
 
 function ShowName(props) {
-    return (<h3> {props.name} </h3>);
+  return (<h3 className = "Box"> {props.name} </h3>);
 }
 
 class CreateButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOpened : false,
-            name : this.props.name,
-            truth : false
-        };
+  constructor(props) {
+      super(props);
+      this.state = {
+          isOpened : false,
+          name : this.props.name,
+          truth : false
+      };
 
-        this.handleClick = this.handleClick.bind(this);
-    }
- 
-    handleClick() {
-        this.setState({
-            truth : this.props.truth,
-            isOpened : true
-        });
-    }
-    
-    render() {
-        return (
-            <div>
-                <button 
-                style = {{
-                    backgroundColor: "#4CAF50", /* Green */
-                    border: "none",
-                    color: "white",
-                    textAlign: "center",
-                    padding: "15px 32px",
-                    textDecoration: "none",
-                    fonSize: "16px"
-                }} 
-                onClick={this.handleClick}>
-                    {!this.state.isOpened ? "Open" : "Opened"}
-                </button>
-                <ShowName name = {
-                    this.state.isOpened && this.state.truth ? "It is here" : "_"
-                } />
-            </div>
-        );
-    }
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+      this.setState({
+          truth : this.props.truth,
+          isOpened : true
+      });
+  }
+  
+  render() {
+      return (
+          <div className= 'Container'>
+              <button className = "OpenButton"
+              onClick={this.handleClick}>
+                  {!this.state.isOpened ? "Open" : "Opened"}
+              </button>
+              <ShowName name = {
+                  this.state.isOpened && this.state.truth ? "It is here" : "_"
+              } />
+          </div>
+      );
+  }
 }
 
-ReactDOM.render(<App name = "Find the Cat"/>, document.getElementById('root'));
+function App() {
+  return (
+    <div>
+    <div className = "Circle"></div>
+    <div class="Cloud"></div>
+    <div class="Cloud2"></div>
+    <FindtheCat name = "Find the Cat"/>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
